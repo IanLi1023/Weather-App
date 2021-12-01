2ece292a9a828aeae0c61384f26b68be22dee2c1
Ian Li <ian.li1023@gmail.com>
2021-12-01 00:19:32 -0700

    created buttons for search city and printed to page

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,41 +1,47 @@
 $(document).ready(function(){
 
-const formEl = $('#city-search');
-const cityDisplayEl = $("#city-search-display");
-const cityInputEl = $("#city-input")
-
-const printSearchedCities = function (cityInput) {
-
-    const citySearchedEl = $('<h1>');
-    citySearchedEl.addClass('col-12 col-sm-4 col-md-3');
-    console.log("Kobe")
-
-    const cardEl = $('<h1>');
-    cardEl.addClass('card h-100');
-    cardEl.append(cityInputEl);
-    console.log("Bryant");
-
-    // const cityName = $('<h5>').addClass('card-header').text(cityInput);
-    // cityName.append(cardEl);
-
-    const cardBodyEl = $('<div>');
-    cardBodyEl.addClass('card-body');
-    cardBodyEl.append(cardEl);
-
-
-    cityDisplayEl.append(cardBodyEl);
-};
-
-const handleFormSubmit = function (event) {
-    // event.preventDefault();
+  const formEl = $('#city-search');
+  const cityDisplayEl = $("#city-search-display");
+  const cityInputEl = $("#city-input")
+  const mainContentEl = $("#main-content")
   
-    const cityInput = cityInputEl.val();
-    printSearchedCities(cityInputEl);
+  const printSearchedCities = function (cityInput) {
   
-    // reset form
-    // cityInputEl.val('<div>');
-
+      const citySearchedEl = $('<div></div>');
+      citySearchedEl.append(cityInput);
+      citySearchedEl.addClass('col-12 btn btn-secondary btn-lg mb-2');
+      console.log("Kobe");
+      
+      cityDisplayEl.append(citySearchedEl);
+  
+      // alert("printing");
+  
+      // const cardEl = $('<h1>');
+      // // cardEl.addClass('card h-100');
+      // cardEl.append(cityInput);
+      // console.log("Bryant");
+  
+      // const cityName = $('<h5>').addClass('card-header').text(cityInput);
+      // cityName.append(cardEl);
+  
+      // const cardBodyEl = $('<li>');
+      // cardBodyEl.addClass('card-body');
+      // cardBodyEl.append(cardEl);
+  
+  
+      // mainContentEl.append(cardBodyEl);
   };
   
-  formEl.on('submit', handleFormSubmit);
-})
+  const handleFormSubmit = function (event) {
+      event.preventDefault(); // This needs to be called, or else the submit button will refresh the page.
+    
+      const cityInput = cityInputEl.val();
+      printSearchedCities(cityInput);
+    
+      // reset form
+      // cityInputEl.val('<div>');
+  
+    };
+    
+    formEl.on('submit', handleFormSubmit);
+  })
